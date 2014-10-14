23aa5fd507e6ccc69261e3b622b53ba609f6748f
ichhass <ichhass@gmail.com>
2014-10-14 04:27:41 +0400

    second commit

--- lesson3/homework/sendError.js
+++ lesson3/homework/sendError.js
@@ -1,23 +1,25 @@
 /**
  * Created on 14.10.14.
  */
+var http = require('http');
+
 function errorHandler(code, res) {
 	switch (code) {
 		case 400:
 			res.statusCode = 400;
-			res.end('Bad request');
+			res.end(http.STATUS_CODES[code]);
 			break;
 		case 405:
 			res.statusCode = 405;
-			res.end('Method nott Allowed');
+			res.end(http.STATUS_CODES[code]);
 			break;
 		case 11000:
 			res.statusCode = 500;
-			res.end('duplicate key error');
+			res.end(http.STATUS_CODES[code]);
 			break;
 		default:
 			res.statusCode = 500;
-			res.end('Unknown error: ' + code);
+			res.end('Error: ' + code);
 	}
 };
 
--- lesson3/homework/server.js
+++ lesson3/homework/server.js
@@ -2,14 +2,15 @@
  * Created on 14.10.14.
  *
  *  curl --data '{"name":"Egor","age":26}' 127.0.0.1:3000/user
+ *  curl -X "DELETE" 127.0.0.1:3000/user/Egor
+ *  curl 127.0.0.1:3000/user/Egor
  *
  */
 var http = require('http');
 var url = require('url');
 var path = require('path');
-var querystring = require('querystring');
 
-var errorHandler = require('./errorHandler');
+var sendError = require('./sendError');
 var User = require('./user');
 
 module.exports = http.createServer(function(req, res) {
@@ -22,20 +23,22 @@ module.exports = http.createServer(function(req, res) {
 				sendUser(url.parse(req.url).pathname.split(path.sep)[2], res);
 				break;
 			case 'DELETE':
-				deleteUser();
+				deleteUser(url.parse(req.url).pathname.split(path.sep)[2], res);
 				break;
 			default:
-				errorHandler(405, res);
+				sendError(405, res);
 		}
 	} else {
-		errorHandler(400, res);
+		sendError(400, res);
 	}
 
 });
 //{"name":"Egor","age":26}
+
 function addUser(req, res) {
-	User.remove({});
+
 	var data = '';
+
 	req
 		.on('readable', function() {
 			data += req.read();
@@ -44,23 +47,22 @@ function addUser(req, res) {
 
 			var user = new User(JSON.parse(data));
 
-			console.log(user);
-
 			user.save(function(err, user) {
-				console.log(err);
 				if (err) {
 					if (err.name != "ValidationError") {
-						errorHandler(err.code, res);
+						console.log(err);
+						sendError(err.code, res);
 						return;
 					}
 
 					var response = {};
+
 					for (var key in err.errors) {
 						response[key] = err.errors[key].message;
 					}
 
 					console.log(response);
-					errorHandler(err.name, res);
+					sendError(err.name, res);
 					return;
 				}
 
@@ -75,15 +77,24 @@ function sendUser(userName, res) {
 	User.find({name: userName}, function(err, user) {
 		if (err) {
 			console.log(err);
-			errorHandler(err.code);
-		} else {
-			console.log(user);
-			res.end(JSON.stringify(user));
+			sendError(err.code);
+			return;
 		}
-	})
+
+		res.end(JSON.stringify(user));
+
+	});
 }
 
-function deleteUser() {
+function deleteUser(userName, res) {
+	User.remove({name: userName}, function(err, user) {
+		if (err) {
+			console.log(err);
+			sendError(err.code);
+			return;
+		}
 
+		res.end('Delete complete');
+	})
 }
 
